79f2ee820f73a7f752d0238a442ec80f3ba13d86
Deepthievijayan <deepthi.e@lxisoft.com>
2018-08-13 16:04:18 +0530

    LI-113

---
+++ Quiz Project/Quiz1/com/lxisoft/quizz/Tdd.java
@@ -0,0 +1,12 @@
+package com.lxisoft.quizz;
+import com.lxisoft.quizz.quest.QuizController;
+import java.io.*;
+import java.util.*;
+public class Tdd
+{
+	
+	public static void main(String args[])
+	{
+		QuizController quiz = new QuizController();	
+	}
+}
---
+++ Quiz Project/quizzz/com/lxisoft/quiz/Tdd.java
@@ -0,0 +1,16 @@
+package com.lxisoft.quiz;
+import com.lxisoft.quiz.quizGame.Quiz;
+import java.io.*;
+import java.util.*;
+public class Tdd
+{
+	
+	public static void main(String args[])
+	{
+		System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<WELCOME>>>>>>>>>>>>>>>>>>>>>\n");
+		System.out.println(" ---------------------QUIZ--------------------");
+		Quiz quiz = new Quiz();
+		
+		
+	}
+}
